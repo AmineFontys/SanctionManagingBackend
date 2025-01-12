74006bc9d0b9eb0019ec250308a1f33cbccfc060
Amine El-Mohamadi <466497@student.fontys.nl>
2025-01-12 20:32:32 +0100

    changing directory

---
+++ SanctionManagingBackend.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace SanctionManagingBackend.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
